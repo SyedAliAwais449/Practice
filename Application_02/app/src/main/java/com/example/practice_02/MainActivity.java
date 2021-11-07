16caaf03d2254b8727606d44d257d164c6800abd
SyedAliAwais449 <saliawaisd449@gmail.com>
2021-11-07 19:19:14 +0500

    Activity Life Cycle

--- Application_02/app/src/main/java/com/example/practice_02/MainActivity.java
+++ Application_02/app/src/main/java/com/example/practice_02/MainActivity.java
@@ -5,16 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("t1", " onCreate Activity Main");
     }
-    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("t1", "onStart Activity Main");
+    }
+    @Override
+    protected void onResume() {
+        super. onResume();
+        Log.d("t1", " onResume Activity Main");
+    }
+    @Override
+    protected void onPause() {
+        super. onPause();
+        Log.d("t1", " onPause Activity Main");
+    }
+    @Override
+    protected void onStop() {
+        super. onStop();
+        Log.d("t1", " onStop Activity Main");
+    }
+    @Override
+    protected void onDestroy() {
+        super. onDestroy();
+        Log.d("t1", " onDestroy Activity Main");
+    }
     public void onClick(View v){
         Intent intent;
 
@@ -30,7 +56,30 @@ public class MainActivity extends AppCompatActivity {
                 intent= new Intent(Intent.ACTION_DIAL, uri);
                 startActivity(intent);
                 break;
-
+            case R.id.button4:
+                String message= "Syed Ali Awais from pucit, bsef18a536";
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+                startActivity(intent);
+                break;
+            case R.id.button5:
+                String URL= "https://github.com/SyedAliAwais449/Practice";
+                Uri webpage = Uri.parse(URL);
+                intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+                break;
+            case R.id.button6:
+                String address= "bsef18a512@pucit.edu.pk";
+                String subject= "Checking email functionality";
+                intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL, address);
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
         }
     }
 }
--- Application_02/app/src/main/java/com/example/practice_02/MainActivity2.java
+++ Application_02/app/src/main/java/com/example/practice_02/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.d("t1", " onCreate Activity Main2");
         textView= findViewById(R.id.textView2);
         btn1= findViewById(R.id.button3);
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +26,30 @@ public class MainActivity2 extends AppCompatActivity {
                 textView.setText(intent.getStringExtra("userId"));
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("t1", "onStart Activity Main2");
+    }
+    @Override
+    protected void onResume() {
+        super. onResume();
+        Log.d("t1", " onResume Activity Main2");
+    }
+    @Override
+    protected void onPause() {
+        super. onPause();
+        Log.d("t1", " onPause Activity Main2");
+    }
+    @Override
+    protected void onStop() {
+        super. onStop();
+        Log.d("t1", " onStop Activity Main2");
+    }
+    @Override
+    protected void onDestroy() {
+        super. onDestroy();
+        Log.d("t1", " onDestroy Activity Main2");
     }
 }
